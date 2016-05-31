d1767c153034cdb1e7078af8338ceed9a92e011d
Anders Papitto <anderspapitto@gmail.com>
2016-05-31 00:31:32 -0700

    cleanups

--- nix.nix
+++ nix.nix
@@ -1,20 +1,17 @@
 { config, pkgs, ... }:
 
 { nix = {
-    trustedBinaryCaches = [  "https://nixcache.reflex-frp.org" "https://hydra.cryp.to" ];
-    binaryCachePublicKeys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
-
     buildCores = 0;
     daemonIONiceLevel = 4;
     daemonNiceLevel = 10;
     maxJobs = 8;
     extraOptions = ''
       auto-optimise-store = true
+      gc-keep-outputs = true
     '';
     nixPath = [ "/etc/nixos" "nixos-config=/etc/nixos/configuration" ];
-    useChroot = true;
-
+    useSandbox = true;
   };
 
-  system.stateVersion = "16.03";
+  system.stateVersion = "16.09";
 }
