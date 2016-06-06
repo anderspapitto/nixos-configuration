9c06eb1e79efd5441f704ca0d7144bca458f85ee
Anders Papitto <anderspapitto@gmail.com>
2016-06-05 20:04:14 -0700

    moved to nixup

--- laptop-base.nix
+++ laptop-base.nix
@@ -12,23 +12,6 @@
     '';
   };
 
-  systemd.user.services.battery_check = {
-    description = "Send notification if battery is low";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = pkgs.writeScript "battery_check" ''
-        #!${pkgs.bash}/bin/bash
-        . ${config.system.build.setEnvironment}
-        . <(udevadm info -q property -p /sys/class/power_supply/BAT0 |
-            grep -E 'POWER_SUPPLY_(CAPACITY|STATUS)=')
-        if [[ $POWER_SUPPLY_STATUS = Discharging && $POWER_SUPPLY_CAPACITY -lt 15 ]];
-        then notify-send -u critical "Battery is low: $POWER_SUPPLY_CAPACITY";
-        fi
-      '';
-    };
-    startAt = "*:00/5";
-  };
-
   users.extraUsers.anders = {
     isNormalUser = true;
     uid = 1000;
