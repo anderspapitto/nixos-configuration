1a7d5db7caff6865f5dc6f0c94d3457c61cef811
Anders Papitto <anderspapitto@gmail.com>
2016-07-09 15:22:47 -0700

    jackdbus fixes

--- audio.nix
+++ audio.nix
@@ -21,39 +21,43 @@ in {
 
   environment.systemPackages = with pkgs; [ jack2Full ];
 
+  environment.etc.jackdrc.text = ''
+    ${pkgs.jack2Full}/bin/jackdbus -dalsa -r48000 -p1024 -n2 -D -Chw:PCH,0 -Phw:PCH,0
+  '';
+
   systemd.user.services = {
-     jackdbus = {
-       description = "Runs jack, and points pulseaudio at it";
-#       wantedBy = [ "default.target" ];
-       requires = [ "pulseaudio.service" ];
-       serviceConfig = {
-         Type = "oneshot";
-         ExecStart = pkgs.writeScript "start_jack" ''
-           #! ${pkgs.bash}/bin/bash
-           . ${config.system.build.setEnvironment}
+    jackdbus = {
+      description = "Runs jack, and points pulseaudio at it";
+#      wantedBy = [ "default.target" ];
+      requires = [ "pulseaudio.service" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = pkgs.writeScript "start_jack" ''
+          #! ${pkgs.bash}/bin/bash
+          . ${config.system.build.setEnvironment}
 
-           ${pkgs.jack2Full}/bin/jack_control start
-           ${pkgs.jack2Full}/bin/jack_control dps device hw:PCH,0
-           ${pulse.out}/bin/pacmd set-default-sink jack_out
-           ${pulse.out}/bin/pacmd set-default-source jack_in
+          ${pkgs.jack2Full}/bin/jack_control start
+          ${pkgs.jack2Full}/bin/jack_control dps device hw:PCH,0
+          ${pulse.out}/bin/pacmd set-default-sink jack_out
+          ${pulse.out}/bin/pacmd set-default-source jack_in
 
-           SINK=$( ${pulse.out}/bin/pacmd list-sinks |
-                   ${pkgs.gnugrep}/bin/grep -oE 'alsa_output.*analog-stereo')
-           ${pulse.out}/bin/pactl suspend-sink $SINK 1
-         '';
-         ExecStop = pkgs.writeScript "stop_jack" ''
-           #! ${pkgs.bash}/bin/bash
-           . ${config.system.build.setEnvironment}
+          SINK=$( ${pulse.out}/bin/pacmd list-sinks |
+                  ${pkgs.gnugrep}/bin/grep -oE 'alsa_output.*analog-stereo')
+          ${pulse.out}/bin/pactl suspend-sink $SINK 1
+        '';
+        ExecStop = pkgs.writeScript "stop_jack" ''
+          #! ${pkgs.bash}/bin/bash
+          . ${config.system.build.setEnvironment}
 
-           SINK=$( ${pulse.out}/bin/pacmd list-sinks |
-                   ${pkgs.coreutils}/bin/grep -oE 'alsa_output.*analog-stereo')
-           ${pulse.out}/bin/pactl suspend-sink $SINK 0
+          SINK=$( ${pulse.out}/bin/pacmd list-sinks |
+                  ${pkgs.coreutils}/bin/grep -oE 'alsa_output.*analog-stereo')
+          ${pulse.out}/bin/pactl suspend-sink $SINK 0
 
-           ${pkgs.jack2Full}/bin/jack_control stop
-         '';
-         RemainAfterExit = true;
-       };
-       environment = { DISPLAY = ":0"; };
-     };
+          ${pkgs.jack2Full}/bin/jack_control stop
+        '';
+        RemainAfterExit = true;
+      };
+      environment = { DISPLAY = ":${toString config.services.xserver.display}"; };
+    };
   };
 }
