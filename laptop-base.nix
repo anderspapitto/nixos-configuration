3b91038fb897618abb0561565247ce4485163fc0
Anders Papitto <anderspapitto@gmail.com>
2016-07-10 14:21:49 -0700

    remove another user service

--- laptop-base.nix
+++ laptop-base.nix
@@ -12,13 +12,13 @@
     '';
   };
 
-  systemd.user.services.battery_check = {
+  systemd.services.battery_check = {
     description = "Send notification if battery is low";
     serviceConfig = {
       Type = "oneshot";
+      User = "anders";
       ExecStart = pkgs.writeScript "battery_check" ''
-        #!${pkgs.bash}/bin/bash
-        . ${config.system.build.setEnvironment}
+        #!${pkgs.bash}/bin/bash --login
         . <(udevadm info -q property -p /sys/class/power_supply/BAT0 |
             grep -E 'POWER_SUPPLY_(CAPACITY|STATUS)=')
         if [[ $POWER_SUPPLY_STATUS = Discharging && $POWER_SUPPLY_CAPACITY -lt 15 ]];
@@ -26,6 +26,7 @@
         fi
       '';
     };
+    environment = { DISPLAY = ":${toString config.services.xserver.display}"; };
     startAt = "*:00/5";
   };
 
