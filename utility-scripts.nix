c6783d6d40062e842eeb1a17b8d0c69a69cfa32b
Anders Papitto <anderspapitto@gmail.com>
2016-11-21 13:11:18 -0800

    updates

--- utility-scripts.nix
+++ utility-scripts.nix
@@ -3,11 +3,12 @@
 let utility-scripts-src = pkgs.fetchFromGitHub {
       owner = "anderspapitto";
       repo = "utility-scripts";
-      rev = "4501836598b5367227167624f09bdb8b3d24c6ac";
-      sha256 = "1zq1rixxrrdk87ab2m92ai9ndibxps1dk3pm25rsfx218287z56m";
+      rev = "31015d2b61cba4dca2385eb983909b4249c3dff9";
+      sha256 = "1mzhl3vdi6bvhx5wr83bzqamhx7qd9a4fk1lj8h48jr2swar1vqh";
     };
 in {
     environment.systemPackages = [
       (pkgs.callPackage "${utility-scripts-src}/nix-shell-wrapper" { })
+      (pkgs.callPackage "${utility-scripts-src}/i3-focus" { })
     ];
 }
